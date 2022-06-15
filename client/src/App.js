117170545a6811a88cb4dea027a26fcdbd180d6c
calebmurrayofficial <calebmurrayofficial@gmail.com>
2022-06-15 16:11:53 -0700

    added navbar and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App m-2">
+      <Navbar />
     </div>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from './favicon.ico'
 
 function Navbar() {
   return (
-    <div className="Navbar">
-
-    </div>
+    <nav className="relative container mt-4 md:mx-auto">
+        <div className='flex items-center justify-between'>
+            <a href='/'><img src={logo} /></a>
+            <div className='flex space-x-6'>
+                <a href='/'>projects</a>
+                <a href='/'>about</a>
+                <a href='/'>contact</a>
+            </div>
+        </div>
+    </nav>
   );
 }
 
